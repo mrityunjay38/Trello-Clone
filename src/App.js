8bb13ca63c40f742ada898a7086fd95efaaa9e73
MRITYUNJAY <mrityunjay.1@mountblue.io>
2019-10-06 19:19:27 +0530

    Adds boards section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import logo from './data/img/logo.png';
 import './App.css';
+import Boards from "./components/boards"
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         </header>
+        <section className="Boards-list">
+        <ul>
+        <Route path="/" exact component={Boards}/>
+        </ul>
+        </section>
     </div>
+    </Router>
   );
 }
 
